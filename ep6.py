764e65aeee0aba43c09051ad4e9f1dd4f0f0cdfc
Maxarus <ConnorBennette@Connors-MacBook-Pro.local>
2017-01-04 16:28:20 -0800

    Completed ep6.py

--- ep6.py
+++ ep6.py
@@ -1,14 +1,16 @@
 #declares variables
 #adds 10 in to the variable x string
 x = "There are %d types of people." % 10
-#decalres binary
+#declares binary
 binary = "binary"
 do_not = "don't"
 y = "Those who know %s and those who %s." % (binary, do_not)
 
 #prints x and y
 #bad joke those who know binary and those who don't
+#one string within a string
 print x
+#two strings within a string
 print y
 
 #Another way of saying there are 10 types of people
@@ -21,6 +23,7 @@ hilarious = False
 joke_evaluation = "Isn't that joke so funny?! %r"
 
 #confirms, not funny
+#String within a string
 print joke_evaluation % hilarious
 
 #declaes two sides of a string
@@ -28,4 +31,5 @@ w = "This is the left side of..."
 e = "a string with a right side."
 
 #adding strings simply concatenates them
+#two strings within a string
 print w + e
