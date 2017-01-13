048c033d090aec81e153fb0d9f4927677d5c8302
Maxarus <ConnorBennette@Connors-MacBook-Pro.local>
2017-01-12 23:02:33 -0800

    worlds smallest most annoying chatbot

---
+++ ep12b.py
@@ -0,0 +1,14 @@
+    #age = raw_input("How old are you? ")
+#height = raw_input("How tall are you? ")
+#weight = raw_input("How much do you weigh? ")
+
+#print "So, you're %r old, %r tall and %r heavy." % (
+#    age, height, weight)
+
+
+while 1:
+    jab1 = raw_input("what you call me?? ")
+    print "no, you're a " + jab1
+    #jab2 = raw_input("no you're a " + jab1)
+    #print "I know you are, but what am I?"
+    #print ""
